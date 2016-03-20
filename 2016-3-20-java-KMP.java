aef31b326666e7de95116d6cd64e0fd40616076a
zhouzhou <1197236910@qq.com>
2016-03-20 23:30:41 +0800

    3/20/2016

--- 2016-3-20-java-KMP.java
+++ 2016-3-20-java-KMP.java
@@ -39,6 +39,56 @@ public class KMP
 	}
 }
 
+// This is how to creat a DFA 
+	dfa[pat.charAt(0)][0] = 1;
+		for( int X = 0 ; j = 1; j < M ; j++)
+		{
+			for(int c = 0;c < R; c++)
+				dfa[c][j] = dfa[c][X];
+			dfa[pat.charAt(j)][j] = j+1;
+			X = dfa[pat.charAt(j)][X];
+		}
 
 
+//now We will discuess another algs:
+//This one called  Boyer-Moore algorithms:
 
+public class BoyerMoore
+{
+	private int [] right;
+	private String pat;
+	BoyerMoore(String pat)
+	{	//计算跳跃表
+		this.pat =pat;
+		int M = pat.length();
+		int R = 256;
+		right = new int[R];
+		for(int c = 0; c<R ;c++)
+			right[c]=-1;
+		for(int j = 0; j < M;j++)
+			right[pat.charAt(j)] = j;
+	}
+
+	public int search(String txt)
+	{	//在TXT中查找字符串和文本匹配的模式字符串
+		int N = txt.length();
+		int M =pat.length();
+		int skip;
+		for( int j = M-1; j >=N-M; i += skip)
+		{	//模式字符串和文本在i位置能够匹配么？
+			skip = 0;
+			for(int j = M-1; j >= 0; j--)
+				if(pat.charAt(j) != txt.charAt(i+j))
+				{
+					skip = j - right[txt.charAt(i+j)];
+					if(skip < 1) skip =1;
+					break;
+				}
+			if( skip == 0 ) return i ;
+		}
+		return N;
+	}
+	public static void main (String[] args)
+	{
+	}
+}
