9d3d742de5f5335ac1cfe5bd7e92b1c27ce4ee13
zhouzhou <1197236910@qq.com>
2016-03-21 23:08:32 +0800

    3/22/2016

---
+++ 2016-3-22-B-tree.java
@@ -0,0 +1,17 @@
+/*   This is The API of a B- tree
+	public class	 Page<key>
+			 Page(boolean bottom)
+		void 	close()
+       		void 	add(Key key)
+		void	add(Page p)
+		
+	     boolean 	isExternal()
+	     boolean 	contains(Key key)
+	        Page    next(key key)
+    	     boolean 	isFull()
+		Page	split()
+   	Iterable<key>	keys()
+
+*/
+
+
