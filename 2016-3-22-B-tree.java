2f7cdad282d6c494dcc1eb1812e890f12a0c795d
zhouzhou <1197236910@qq.com>
2016-03-21 23:18:47 +0800

    3/22/2016

--- 2016-3-22-B-tree.java
+++ 2016-3-22-B-tree.java
@@ -14,4 +14,31 @@
 
 */
 
+//Now I will write some functions
+//like add(Key key); add(Page p)
+
+
+public class BTreeSET<key extends Comparable <key>>
+{	
+	private Page root = new Page(ture);
+
+	public BTreeSET(key sentinel)
+	{	add(sentinel);	}
+
+	public boolean contains(Key key)
+	{	return contains(root, key);	}
+
+	private boolean contains(Page h , Key key)
+	{
+		if (h.isExternal())	return h.contains(key);
+		return contains(h.next(key), key);
+	}
+
+	public void add(Key key)
+	{
+		add(root, key)
+		if(root.isFull())
+		{
+			Page lethalf = root;
+
 
