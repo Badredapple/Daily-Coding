28608cd86458689ba7a9a73773ef6393da57d4dc
zhouzhou <1197236910@qq.com>
2016-03-24 14:43:52 +0800

    3/24/2016 算法

---
+++ Labeling-Balls.cpp
@@ -0,0 +1,18 @@
+//POJ 3687 Labeling Balls(拓扑排序)
+/*		
+	题目：给定m个球的关系，求出具有最小字典序列的各个球的重量
+	
+*/
+
+#include <isotream>
+#include <cstdio>
+#include <algorithm>
+#include <memory.h>
+#include <cmath>
+#include <bitset>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+const int BORDER = (1<<20)-1;
