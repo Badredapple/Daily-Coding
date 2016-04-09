38501991b583feecddbef2732e2466633e2649e0
1197236910@qq.com <1197236910@qq.com>
2016-04-09 15:10:06 +0800

    This is learning python in Black_hat_python

--- 2016-4-8-TCPclient.py
+++ 2016-4-8-TCPclient.py
@@ -7,3 +7,12 @@ target_port = 80
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #connect the client
+client.connect((target_host, target_port))
+
+#send some data
+client.send("GET / HTTP/1.1\r\nHost:www.baidu.com\r\n\r\n")
+
+#receive some data
+response = client.recv(4096)
+
+print response
